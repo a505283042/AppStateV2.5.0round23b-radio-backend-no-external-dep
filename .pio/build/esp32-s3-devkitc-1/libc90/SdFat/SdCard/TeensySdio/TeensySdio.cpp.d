e1f811e6bdc066de548f9b39c79079b970e728cc
a505283042 <a505283042@gmail.com>
2026-04-06 14:33:57 +0800

    build: 添加构建产物和依赖文件
    添加项目构建过程中生成的二进制文件、依赖文件和编译输出文件。包括固件文件、字体文件和各种平台相关的驱动文件。

---
+++ .pio/build/esp32-s3-devkitc-1/libc90/SdFat/SdCard/TeensySdio/TeensySdio.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32-s3-devkitc-1/libc90/SdFat/SdCard/TeensySdio/TeensySdio.cpp.o: \
+ .pio/libdeps/esp32-s3-devkitc-1/SdFat/src/SdCard/TeensySdio/TeensySdio.cpp
---
+++ .pio/build/esp32-s3-devkitc-1/libc90/SdFat/common/upcase.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-s3-devkitc-1/libc90/SdFat/common/upcase.cpp.o: \
+ .pio/libdeps/esp32-s3-devkitc-1/SdFat/src/common/upcase.cpp \
+ .pio/libdeps/esp32-s3-devkitc-1/SdFat/src/common/upcase.h
